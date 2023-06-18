0297d4ab1cde3a9326e13cd24b59ec9e4191d3cc
吴安之 <2443077826@qq.com>
2023-06-19 00:25:18 +0800

    添加游戏启动入口脚本

---
+++ Client/AzoneClient/Assets/AzoneFramework/Core/Boot/GameBoot.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 游戏启动入口
+/// </summary>
+public class GameBoot : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("游戏程序启动。");
+    }
+}
